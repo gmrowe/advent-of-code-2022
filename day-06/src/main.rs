cca0765ab770f2c2e4b884cae15b64edf3e970f6
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-12-09 10:05:18 -0700

    Implement day-06 part-2

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,11 +1,10 @@
 use std::{fs, io};
 
-fn part_1(input: &str) -> usize {
-    const SEG_LEN: usize = 4;
+fn find_duplicates(input: &str, seg_len: usize) -> usize {
     let mut index_found = false;
     let mut i = 0;
-    while i < input.len() - SEG_LEN + 1 && !index_found {
-        let segment = &input[i..i + SEG_LEN];
+    while i < input.len() - seg_len + 1 && !index_found {
+        let segment = &input[i..i + seg_len];
         let mut duplicate_found = false;
         let mut j = 0;
         while j < segment.len() && !duplicate_found {
@@ -22,14 +21,23 @@ fn part_1(input: &str) -> usize {
         index_found = !duplicate_found;
         i += 1
     }
-    i - SEG_LEN - 1
+    i + seg_len - 1
+}
+
+fn part_1(input: &str) -> usize {
+    find_duplicates(input, 4)
+}
+
+fn part_2(input: &str) -> usize {
+    find_duplicates(input, 14)
 }
 
 fn main() -> io::Result<()> {
-    const FILE_PATH: &str = "input.txt";
-    let input = fs::read_to_string(FILE_PATH)?;
-
+    let input = fs::read_to_string("input.txt")?;
     let result_1 = part_1(&input);
     println!("day-06;part-1 = {}", result_1);
+
+    let result_2 = part_2(&input);
+    println!("day-06;part-2 = {}", result_2);
     Ok(())
 }
