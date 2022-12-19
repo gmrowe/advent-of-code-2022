763d5fd2f04a343fcdc5d262764d2fbbdf154174
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-12-18 17:37:25 -0700

    Implement day-10/part-2

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,8 +1,6 @@
 use std::{fs, io};
 
-fn main() -> io::Result<()> {
-    const FILE_PATH: &str = "input.txt";
-    let input = fs::read_to_string(FILE_PATH)?;
+fn build_cycle_log(input: &str) -> Vec<i32> {
     let mut register = 1;
     let mut cycle_log = Vec::new();
     cycle_log.push(0);
@@ -23,7 +21,11 @@ fn main() -> io::Result<()> {
             unreachable!("Malformed input line: {line}");
         }
     }
+    cycle_log
+}
 
+#[allow(dead_code)]
+fn part_1(cycle_log: &[i32]) -> i32 {
     const INIT_INDEX: usize = 20;
     const FINAL_INDEX: usize = 220;
     const STEP: usize = 40;
@@ -33,6 +35,35 @@ fn main() -> io::Result<()> {
         let signal_strength = idx as i32 * cycle_value;
         signal_strength_sum += signal_strength;
     }
-    println!("day-10/part-1 = {signal_strength_sum}");
+    signal_strength_sum
+}
+
+fn part_2(cycle_log: &[i32]) -> String {
+    const LINE_WIDTH: usize = 40;
+    let mut result = String::new();
+    for screen_line in cycle_log.chunks(LINE_WIDTH) {
+        for (pixel_num, cycle) in screen_line.iter().enumerate() {
+            let pixel_range = pixel_num as i32 - 1..=pixel_num as i32 + 1;
+            if pixel_range.contains(cycle) {
+                result.push('#');
+            } else {
+                result.push(' ');
+            }
+        }
+        result.push('\n');
+    }
+    result
+}
+
+fn main() -> io::Result<()> {
+    const FILE_PATH: &str = "input.txt";
+    let input = fs::read_to_string(FILE_PATH)?;
+    let cycle_log = build_cycle_log(&input);
+
+    let result_1 = part_1(&cycle_log);
+    println!("day-10/part-1 = {result_1}");
+
+    let result_2 = part_2(&cycle_log);
+    println!("day-10/part-2=\n{result_2}");
     Ok(())
 }
